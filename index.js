80d5cb2cd6321d018a3ca2997f4ff2c1a34eacca
kristjanjansen <kristjan.jansen@gmail.com>
2019-01-09 17:22:25 +0200

    rename

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as components from "https://designstem.github.io/framework/framework.js";
-import * as utils from "https://designstem.github.io/framework/utils.js";
+import * as components from "https://designstem.github.io/fachwerk/components.js";
+import * as utils from "https://designstem.github.io/fachwerk/utils.js";
 
 for (const name in components) {
   Vue.component(name, components[name]);
