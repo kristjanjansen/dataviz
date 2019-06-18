44e3719263a66f9c465d39a570a09ec0a1ee0b29
Kristjan Jansen <kristjan.jansen@gmail.com>
2019-06-18 14:01:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import * as components from "https://designstem.github.io/fachwerk/components.js";
-import * as utils from "https://designstem.github.io/fachwerk/utils.js";
+import { components, utils } from "https://designstem.github.io/fachwerk/fachwerk.js";
 
 for (const name in components) {
   Vue.component(name, components[name]);
@@ -28,12 +27,11 @@ new Vue({
   methods: { ...utils },
   template: `
   <div>
-    <f-fetch-data url="./README.md">
-      <f-theme slot-scope="data" theme="blue">
+    <f-fetch-data src="./README.md" v-slot="{ value: content }">
+      <f-theme theme="blue">
         <f-content-slides
-          style="--emphasis: var(--yellow)"
-          :content="data.value"
-          style="height: 100vh; --f-content-slides-padding: calc(var(--base) * 8)  calc(var(--base) * 10);"
+          :content="content"
+          style="--emphasis: var(--yellow); height: 100vh; --f-content-slides-padding: calc(var(--base) * 8)  calc(var(--base) * 10);"
           class="break"
         />
       </f-theme>
