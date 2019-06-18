8f82e86ca05aee53a603a78ff5ac65735c8c8cce
Kristjan Jansen <kristjan.jansen@gmail.com>
2019-06-18 14:49:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { fachwerk } from "https://designstem.github.io/fachwerk/fachwerk.js";
 
-fachwerk({ src: './README.md', style: {
+fachwerk({ src: './README.md', theme: 'blue', style: {
   '--emphasis': 'var(--yellow)',
   height: '100vh',
   '--f-content-slides-padding': 'calc(var(--base) * 8) calc(var(--base) * 10)'
